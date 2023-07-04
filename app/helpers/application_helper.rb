29f6a38a149ba44f7a6a1e6f999c3c0f904ecc3e
Damilare Adepoju <108327427+adamilare@users.noreply.github.com>
2023-07-04 14:47:22 +0100

    Style Category view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def nav_active(page)
     return 'active' if current_page?(page) || request.original_fullpath.start_with?("#{page}/")
   end
+
+  def navbar_icon(some_condition)
+    if some_condition
+      content_tag(:span, '', class: 'navbar-toggler-icon')
+    else
+      content_tag(:span, '', class: 'fa fa-back')
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # get 'home/index'
   devise_for :users
-  resources :spend_transactions, path: 'transactions'
-  resources :spend_categories, path: 'categories'
+  resources :spend_categories, path: 'categories'do
+    resources :spend_transactions, path: 'transactions'
+  end
   get '/welcome', to: 'home#welcome'
   root "spend_categories#index"
 end
