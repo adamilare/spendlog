f9163faee09dc28a52cd87942a0a0b584129075d
Damilare Adepoju <108327427+adamilare@users.noreply.github.com>
2023-07-06 13:49:49 +0100

    Refactor application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,14 +3,6 @@ module ApplicationHelper
     return 'active' if current_page?(page) || request.original_fullpath.start_with?("#{page}/")
   end
 
-  def navbar_icon(some_condition)
-    if some_condition
-      content_tag(:span, '', class: 'navbar-toggler-icon')
-    else
-      content_tag(:span, '', class: 'fa fa-back')
-    end
-  end
-
   def submit_form(form_id, button_id)
     javascript_tag do
       <<~JS
