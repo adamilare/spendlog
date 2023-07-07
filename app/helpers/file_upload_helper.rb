1b4f1a7dc6665ee65b92d50b661ef61ece362c54
Damilare Adepoju <108327427+adamilare@users.noreply.github.com>
2023-07-07 09:35:50 +0100

    Fix render file saving bug

--- app/helpers/file_upload_helper.rb
+++ app/helpers/file_upload_helper.rb
@@ -2,8 +2,23 @@ module FileUploadHelper
   def process_icon_upload(attached_icon)
     return unless attached_icon.present?
 
-    icon_path = Rails.root.join('app', 'assets', 'images', attached_icon.original_filename)
+    unique_basename = generate_uuid_filename(attached_icon.original_filename)
+    icon_path = Rails.root.join('app', 'assets', 'images', unique_basename)
     File.binwrite(icon_path, attached_icon.read)
-    attached_icon.original_filename
+    unique_basename
+  end
+
+  def generate_unique_filename(filename)
+    extension = File.extname(filename)
+    basename = File.basename(filename, extension)
+    unique_basename = "#{basename}-#{SecureRandom.hex(4)}"
+    "#{unique_basename}#{extension}"
+  end
+
+  def generate_uuid_filename(filename)
+    extension = File.extname(filename)
+
+    uuid = SecureRandom.uuid
+    "#{uuid}#{extension}"
   end
 end
